1b7f0805594de0e46a695942555d8d677563ba45
Tiago Candeias <tiago.g.candeias@gmail.com>
2022-05-29 22:46:42 +0100

    Add Discount.API boiler code

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyMicroServices.Discount.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]/")]
+public class DiscountController : ControllerBase
+{
+
+    public DiscountController()
+    {
+        
+    }
+
+}
